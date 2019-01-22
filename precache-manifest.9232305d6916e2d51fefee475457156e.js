d3427db9d655f9692c87ab7ec694eb92757b1c57
unknown <alfred.osorio@outlook.com>
2019-01-22 18:09:06 +1100

    Updates

---
+++ precache-manifest.9232305d6916e2d51fefee475457156e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "23a2bf0099fe49e05bdcb2bf32ef1dc8",
+    "url": "/og-web/static/media/logo.23a2bf00.png"
+  },
+  {
+    "revision": "16d4c6f5b2eada6575ee1e5ea16871dc",
+    "url": "/og-web/static/media/human_dragons.16d4c6f5.mp3"
+  },
+  {
+    "revision": "5477e82e9aefbbcde97d",
+    "url": "/og-web/static/js/runtime~main.5477e82e.js"
+  },
+  {
+    "revision": "faca84e59a086a12774d",
+    "url": "/og-web/static/js/main.faca84e5.chunk.js"
+  },
+  {
+    "revision": "916b373cfef80c85a28b",
+    "url": "/og-web/static/js/1.916b373c.chunk.js"
+  },
+  {
+    "revision": "faca84e59a086a12774d",
+    "url": "/og-web/static/css/main.2dc62620.chunk.css"
+  },
+  {
+    "revision": "fb2483bc5918c3b5f390246135a65b9e",
+    "url": "/og-web/index.html"
+  }
+];
