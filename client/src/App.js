91bbbc9393ba2a8d34501ca096569d7df2e8acb4
unknown <alfred.osorio@outlook.com>
2019-01-22 15:28:02 +1100

    Add Logo and Coming Soon

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.png'
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Welcome to OG Nerd</h1>
+        {/* Logo */}
+          <div className="Logo-header">
+            <img src={logo} alt='og_nerd logo'></img>
+          </div>
+        {/* Coming Soon */}
+          <div>
+            <p>Coming Soon</p>
+          </div>
         </header>
       </div>
     );
