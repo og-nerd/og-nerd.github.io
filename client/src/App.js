3f8b5cc1e08a37090351e4ffcafd00d6e1ec30b9
unknown <alfred.osorio@outlook.com>
2019-01-22 17:07:53 +1100

    Add footer and social media buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 import logo from './assets/images/logo.png'
 import './App.css';
-import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <body className="App-header">
         {/* Logo */}
           <div className="Logo-header">
             <img src={logo} alt='og_nerd logo'></img>
           </div>
         {/* Coming Soon */}
-          <div>
-            <p>Coming Soon</p>
+          <div className="Coming-soon">
+            <p>COMING SOON</p>
           </div>
-        </header>
+        {/* Footer */}
+          <div className="Footer">
+          {/* Social Media Buttons */}
+            <div className="Social-media-buttons">
+              <i class="fab fa-facebook-f"></i>
+              <i class="fab fa-instagram"></i>
+              <i class="fab fa-youtube"></i>
+            </div>
+            <p>&copy; OG Nerd 2019</p>
+          </div>
+        </body>
       </div>
     );
   }
